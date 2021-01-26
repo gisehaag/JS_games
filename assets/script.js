bd389daeefc795d06cf504edcdad11bd5660e902
Gise Haag <ing.giselahaag@gmail.com>
2021-01-26 17:24:27 +0100

    Tweak game score & create assingElements function

--- assets/script.js
+++ assets/script.js
@@ -5,58 +5,86 @@ class Game {
 
 		// this.config = Object.assign(configDefaults, config);
 		this.game = document.querySelector('.game');
+		this.giseTurn = false;
+
+		this.movesDone = [];
+		this.allCells = ['a1', 'b1', 'c1', 'a2', 'b2', 'c2', 'a3', 'b3', 'c3'];
+
+		this.myMoves = {
+			cell: [],
+			columns: {
+				"a": 0,
+				"b": 0,
+				"c": 0,
+			},
+			rows: {
+				"1": 0,
+				"2": 0,
+				"3": 0,
+			}
+		};
+
+		this.playerMoves = {
+			cell: [],
+			columns: {
+				"a": 0,
+				"b": 0,
+				"c": 0,
+
+			},
+			rows: {
+				"1": 0,
+				"2": 0,
+				"3": 0,
+			}
+		};
+
+		this.assignElements();
+		this.addEvents();
+	}
+
+	assignElements() {
 		this.board = this.game.querySelector('.board');
 		this.message = this.game.querySelector('.message');
 		this.resetButton = this.game.querySelector('.reset');
 		this.cells = this.board.querySelectorAll('.cell');
-
-		this.giseTurn = false;
-		this.moves = [];
-		this.myMoves = [];
-		this.playerMoves = [];
-		this.playerColumns = [];
-		this.myColumns = [];
-		this.playerRows = [];
-		this.myRows = [];
-
-		this.allCells = ['a1', 'b1', 'c1', 'a2', 'b2', 'c2', 'a3', 'b3', 'c3'];
-
-		// this.displayTemplate();
-		this.addEvents();
-
 	}
 
 	addEvents() {
 		this.cells.forEach((cell) => {
-			cell.addEventListener('click', this.round.bind(this))
+			cell.addEventListener('click', this.round.bind(this));
 		})
 	}
 
 	round(e) {
 		let cell = e.currentTarget;
-		let lastMove = cell.dataset.cellid;
+		let lastMove = cell.dataset.cellId;
+		let lastColumn = lastMove[0];
+		let lastRow = lastMove[1];
 
-		if (this.playerMoves.includes(lastMove) || this.myMoves.includes(lastMove)) {
+		if (this.playerMoves.cell.includes(lastMove) || this.myMoves.cell.includes(lastMove)) {
 			alert('Ya jugaste ese casillero, elegí otro porfa 😏');
 			return;
 		}
 
-		this.playerMove(lastMove, cell);
+		this.playerMove(lastMove, lastColumn, lastRow, cell);
+
 		if (this.giseTurn) this.giseMove(lastMove);
 	}
 
 
-	playerMove(lastMove, cell) {
+	playerMove(lastMove, lastColumn, lastRow, cell) {
 		this.giseTurn = false;
 		cell.innerHTML = `<i class="icon-star-empty"></i>`;
-		this.playerMoves.push(lastMove);
-		this.playerColumns.push(cell.dataset.column);
-		this.playerRows.push(cell.dataset.row);
 
-		this.moves.push(lastMove);
+		this.movesDone.push(lastMove);
 
-		if (this.playerMoves.length >= 3) {
-			if (!this.isWinningCase(this.playerColumns, this.playerRows, this.playerMoves)) {
+		this.playerMoves.cell.push(lastMove);
+		this.playerMoves.columns[lastColumn] += 1;
+		this.playerMoves.rows[lastRow] += 1;
+
+		if (this.playerMoves.cell.length >= 3) {
+			if (!this.isWinningCase()) {
 				this.giseTurn = true;
 			};
 		} else {
@@ -69,11 +97,11 @@ class Game {
 		let posibleMove = [];
 		let myMove = '';
 
-		if (!(lastMove == 'b2') && !(this.myMoves.length > 0)) {
+		if (!(lastMove == 'b2') && !(this.myMoves.cell.length > 0)) {
 			myMove = 'b2';
 		} else {
 			this.allCells.forEach((posibility) => {
-				if (!this.moves.includes(posibility)) {
+				if (!this.movesDone.includes(posibility)) {
 					posibleMove.push(posibility);
 				}
 			})
@@ -85,58 +113,46 @@ class Game {
 		let cell = this.board.querySelector(`#${myMove}`);
 
 		cell.innerHTML = `<i class="icon-earth"></i>`;
-		this.moves.push(myMove);
-		this.myMoves.push(myMove);
-		this.myColumns.push(cell.dataset.column);
-		this.myRows.push(cell.dataset.row);
+		this.movesDone.push(myMove);
 
-		if (this.myMoves.length >= 3) {
-			console.log(this.isWinningCase(this.myColumns, this.myRows, this.myMoves));
-		}
+		this.myMoves.cell.push(myMove);
+
+		this.myMoves.columns[cell.dataset.column] += 1;
+		this.myMoves.rows[cell.dataset.row] += 1;
+
+		if (this.myMoves.length >= 3) this.isWinningCase();
 	}
 
 	resetBoard() {
 		this.cells.forEach((cell) => cell.innerHTML = ``);
 	}
 
-	isWinningCase(columns, rows, moves) {
+	isWinningCase() {
+		let winnerMoves = (this.giseTurn) ? this.myMoves : this.playerMoves;
+		let cell = winnerMoves.cell;
+		let columns = winnerMoves.columns;
+		let rows = winnerMoves.rows;
 
-		let columnA = 0;
-		let columnB = 0;
-		let columnC = 0;
-		let row1 = 0;
-		let row2 = 0;
-		let row3 = 0;
+		for (let key in columns) {
+			if (columns[key] == 3) {
+				this.displayMessage();
+				return true;
+			}
+		}
 
-		columns.forEach(column => {
-			if ('a' == column) columnA += 1;
-			if ('b' == column) columnB += 1;
-			if ('c' == column) columnC += 1;
-		})
+		for (let key in rows) {
+			if (rows[key] == 3) {
+				this.displayMessage();
+				return true;
+			}
+		}
 
-		rows.forEach(row => {
-			if ('1' == row) row1 += 1;
-			if ('2' == row) row2 += 1;
-			if ('3' == row) row3 += 1;
-		})
-
-		if (columnA >= 3 ||
-			columnB >= 3 ||
-			columnC >= 3 ||
-			row1 >= 3 ||
-			row2 >= 3 ||
-			row3 >= 3) {
-
+		if (cell.includes('a1') && cell.includes('b2') && cell.includes('c3')) {
 			this.displayMessage();
 			return true;
 		}
 
-		if (moves.includes('a1') && moves.includes('b2') && moves.includes('c3')) {
-			this.displayMessage();
-			return true;
-		}
-
-		if (moves.includes('c1') && moves.includes('b2') && moves.includes('a3')) {
+		if (cell.includes('c1') && cell.includes('b2') && cell.includes('a3')) {
 			this.displayMessage();
 			return true;
 		}
@@ -146,7 +162,6 @@ class Game {
 		let winningMessage = !(this.giseTurn) ? 'Felicitaciones! Me ganaste!' : 'No te preocupes, te doy la revancha!';
 
 		this.message.innerHTML = winningMessage;
-		console.log(this.resetButton.classList);
 		this.resetButton.classList.remove('hidden');
 	}
 }
